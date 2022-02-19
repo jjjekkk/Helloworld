f5ccab36c0ca69e678dc438b51f088fdc88b84a8
JekAnashko <96998019+jjjekkk@users.noreply.github.com>
2022-02-19 16:57:58 +0500

    Update JS_Calc.js

--- JS_Calc.js
+++ JS_Calc.js
@@ -1,7 +1,4 @@
-var buttonPlus = document.getElementById('buttonPlus');
-var buttonMinus = document.getElementById('buttonMinus');
-var buttonMultiplay = document.getElementById('buttonMultiplay');
-var buttonDevide = document.getElementById('buttonDevide');
+var operationButtons = document.getElementsByClassName('operation-button');
 
 var input1 = document.getElementById('number1');
 var input2 = document.getElementById('number2');
@@ -9,7 +6,6 @@ var input2 = document.getElementById('number2');
 function makeOperation(operationCode) {
     var number1 = Number(input1.value);
     var number2 = Number(input2.value);
-2
     if (operationCode === '+') {
         var result = number1 + number2;
     } else if (operationCode === '-') {
@@ -24,18 +20,11 @@ function makeOperation(operationCode) {
     window.alert(result);
 }
     
-
 function onOperationButtonClick (eventObject) {
     var clickedElement = eventObject.currentTarget;
     var operation = clickedElement.innerHTML;
     makeOperation(operation);
 }
-
-
-
-var operationButtons = [buttonPlus, buttonMinus, buttonMultiplay, buttonDevide];
-
-
 
 for (let i = 0; i < operationButtons.length; i++) {
     var button = operationButtons[i];
