b9f850a58674cf43d209d66afca4c1996b45edfe
JekAnashko <96998019+jjjekkk@users.noreply.github.com>
2022-03-15 01:35:23 +0500

    Update startSliders.js

--- startSliders.js
+++ startSliders.js
@@ -1,4 +1,4 @@
-let slider1 = sliderFactory.getNewSlider();
-let slider2 = sliderFactory.getNewSlider();
+let slider1 = new Slider();
+let slider2 = new Slider();
 slider1.start('itk-slider1');
 slider2.start('itk-slider2');
